d8264ba47d12e196ac3690cde1c9740afe05660e
LAKSHMIKAR\laksh <lakshmikarmalasani@gmail.com>
2018-04-10 19:02:43 -0500

    no message

---
+++ UseNuget/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NugetDemo;
+
+namespace UseNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Class1 ndemo = new Class1();
+            int c = ndemo.add(a, b);
+            Console.WriteLine(c);
+            Console.WriteLine(ndemo.subtract(a,b));
+            Console.ReadLine();
+
+        }
+    }
+}
